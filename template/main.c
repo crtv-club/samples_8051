df6ce456039bf5f9527f7e755c67167c92217104
Sergey Kostyuk <contact@s-kostyuk.me>
2018-03-01 15:41:30 +0200

    Added template project

---
+++ template/main.c
@@ -0,0 +1,18 @@
+#include <reg52.h>
+#include "support.h"
+
+void setup() {
+	// Nothing is here yet
+}
+
+void loop() {
+	// Nothing is here yet
+}
+
+void main() {
+	setup();
+	
+	while (1) {
+		loop();
+	}
+}
